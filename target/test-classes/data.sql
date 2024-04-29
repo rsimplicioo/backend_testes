30f1a0ef9a15ede05007edb0b281f6d4473c7b92
rafael.boucher <simplicio.rsb@gmail.com>
2024-04-29 18:01:09 -0300

    `test`: testes para a camada service.

--- target/test-classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO mensagem
-    (ID, USUARIO, CONTEUDO, DATA_CRIACAO, GOSTEI)
-VALUES
-    ('4a3679eb-5f35-497f-b113-fae19fb004ce', 'Miguel', 'Conteudo da mensagem 01', '2024-01-15 00:00:00.10000', 0),
-    ('47aedc7b-972f-4ab5-8d4c-43bace07df5d', 'Jessica', 'Conteudo da mensagem 02', '2024-01-15 00:00:00.20000', 0),
-    ('1d50cccf-e4e0-4a83-bac4-449d6693cfd1', 'Rafael', 'Conteudo da mensagem 03', '2024-01-15 00:00:00.30000', 0);
-
-
